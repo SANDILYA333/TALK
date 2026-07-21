6aa6c86364eb7ba1d03db60a47a48200ed79481c
Sandy <sandilyamachiraju33@gmail.com>
2026-07-21 21:31:18 +0530

    Refactor server startup logic to streamline production job initiation

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,7 +40,5 @@ app.listen(PORT,()=>{
     connectDB();
     console.log("Server is up and Running on PORT:", PORT);
 
-    if(process.env.NODE_ENV ==="production"){
-        job.start()
-    }
+    if (process.env.NODE_ENV === "production") job.start();
 });
