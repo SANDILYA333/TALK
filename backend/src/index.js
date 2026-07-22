9f8a962adb9fd8b5e602f4428fc4b9703a2e6c6d
Sandy <sandilyamachiraju33@gmail.com>
2026-07-22 15:09:41 +0530

    Add authentication routes and middleware for user verification

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,7 @@ import { connectDB } from "./lib/db.js"
 import job from "./lib/cron.js";
 
 import clerkWebhook from "./webhooks/clerk.webhooks.js";
+import authRoutes from "./routes/auth.route.js" 
 
 const app =express()
 const PORT = process.env.PORT
@@ -34,6 +35,8 @@ app.use(clerkMiddleware())
 app.get("/health",(req,res)=>{
     res.status(200).json({ok:true})
 })
+
+app.use("/api/auth",authRoutes)
 
 if(fs.existsSync(publicDir)){
     app.use(express.static(publicDir));
