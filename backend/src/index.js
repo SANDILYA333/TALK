0bc8b24b270a104be69e1e0d7d6753fd2d1d5b65
Sandy <sandilyamachiraju33@gmail.com>
2026-07-22 16:26:48 +0530

    Add socket.io integration for real-time messaging and user presence

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,8 +15,8 @@ import job from "./lib/cron.js";
 import clerkWebhook from "./webhooks/clerk.webhooks.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app,server } from "./lib/socket.js";
 
-const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
@@ -48,7 +48,7 @@ if (fs.existsSync(publicDir)) {
   });
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectDB();
   console.log("Server is up and Running on PORT:", PORT);
 
