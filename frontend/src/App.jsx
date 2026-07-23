ac44358267ecdb894025bde280bf298c194b350c
Sandy <sandilyamachiraju33@gmail.com>
2026-07-23 11:06:01 +0530

    feat: implement authentication routing and add AuthPage component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,24 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-import { Button } from '@heroui/react';
-import { ThemeProvider } from './context/ThemeContext';
-import { WallpaperProvider } from './context/WallpaperContext';
-import { Route,Routes } from 'react-router';
-import {ChatPage} from "./pages/ChatPage.jsx"
+import { ThemeProvider } from "./context/ThemeContext";
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { Navigate,Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage.jsx";
+import AuthPage from "./pages/AuthPage.jsx";
+import { useAuth } from "@clerk/react";
 
 function App() {
+
+  const {isSignedIn , isLoaded} =useAuth();
+  if(!isLoaded) return <p>Loading ...</p>
   return (
-    <ThemeProvider> 
+    <ThemeProvider>
       <WallpaperProvider>
         <Routes>
-          <Route path="/" element={<ChatPage/>}/>
-          <Route path="/" element={<ChatPage/>}/>
+          <Route path="/" element={isSignedIn?<ChatPage />:<Navigate to={"/auth"} replace />} />
+          <Route path="/auth" element={!isSignedIn?<AuthPage />:<Navigate to={"/chat"} replace />} />
         </Routes>
       </WallpaperProvider>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,6 +1,8 @@
 
 function ChatPage() {
-  return <div>ChatPage</div>;
+  return (
+    <div>ChatPage</div>
+  )
 }
 
-export default ChatPage;
+export default ChatPage
