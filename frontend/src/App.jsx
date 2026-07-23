1c3d0e3530eaebe05824fe6d489bd16b2c520f5f
Sandy <sandilyamachiraju33@gmail.com>
2026-07-23 10:30:13 +0530

    feat: add HeroUI components and theme presets
    - Added @heroui/react and @heroui/styles dependencies to package.json.
    - Integrated HeroUI Button component in App.jsx with a sample button.
    - Created herouiThemePresets.js to define various theme presets for HeroUI.
    - Updated index.css to include HeroUI styles and custom dark theme handling.
    - Added heroui-theme-presets.css for specific accent styles based on theme presets.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
 import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Button } from '@heroui/react';
 
 function App() {
   return (
-    <> <h1>MY APP</h1>
+    <> <h1 className="text-4xl text-red-500 bg-amber-300">MY APP</h1>
+
+      <Button>Hello MF</Button>
       <header>
         <Show when="signed-out">
           <SignInButton mode="modal" />
