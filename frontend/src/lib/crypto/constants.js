4e71cc3707de15a3877c990bf7ef58f47b7a3acc
Sandy <sandilyamachiraju33@gmail.com>
2026-09-09 21:29:57 +0530

    feat: increment database version to 5 and consolidate IndexedDB object store initialization logic

--- frontend/src/lib/crypto/constants.js
+++ frontend/src/lib/crypto/constants.js
@@ -13,7 +13,7 @@ export const PUBLIC_KEY_USAGES = [];
 
 export const STORAGE_DB_NAME = "talk_crypto_db";
 export const STORAGE_STORE_NAME = "identity_keys";
-export const STORAGE_DB_VERSION = 1;
+export const STORAGE_DB_VERSION = 5;
 export const DEVICE_IDENTITY_KEY = "device_identity_keypair";
 
 /**
